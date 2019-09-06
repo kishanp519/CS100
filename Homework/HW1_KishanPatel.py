717a114150c32c212d2d385c18f997b38045332b
Kishan Patel <kishanp519@gmail.com>
2019-09-06 00:02:43 -0400

    Adding Exercise 1.2

--- Homework/HW1_KishanPatel.py
+++ Homework/HW1_KishanPatel.py
@@ -56,5 +56,17 @@ print("The colleges I've attended are:", colleges)
 5) - If you have two values with no operator between them, you get a "SyntaxError: invalid syntax" error.
      - Input: '2 2' -- Returned error above.
 ------------------------------------------------------------------------------------------------------------------------
+** Exercise 1.2 **
 
+1) - There are 2562 seconds in 42 minutes and 42 seconds.
+     - Input: '(42 * 60) + 42' -- Returned 2562.
+     
+2) - There are 6.21 miles in 10 kilometers.
+     - Input: '10 / 1.61' -- Returned 6.21.
+     
+3) - Average pace per mile is 6 minutes and 53 seconds.
+   - Input: '(2562 / 6.21) / 60' -- Returned  6.876.
+            '0.876 * 60' -- Returned 52.56.
+
+------------------------------------------------------------------------------------------------------------------------
 """
