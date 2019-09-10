4454a3e3463e7fe9db8fb44ef68f0e58072458dc
Kishan Patel <kishanp519@gmail.com>
2019-09-10 12:41:11 -0400

    Structure Fix

--- Classwork/InitialsAndAge.py
+++ Classwork/InitialsAndAge.py
@@ -13,5 +13,5 @@ lastInitials = last[0] + last[-1]
 futureAge = first + " " + last + ", in 10 years you will be " + str(age + 10) + " years old."
 
 print("")
-print("Your first and last letters of your full name are:", firstInitials + lastInitials)
+print("The first and last letters of your full name are:", firstInitials + lastInitials)
 print(futureAge)
