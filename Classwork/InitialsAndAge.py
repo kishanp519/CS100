9e3344b831d4c10b4b5a14d317856150988aa622
Kishan Patel <kishanp519@gmail.com>
2019-09-10 12:37:20 -0400

    Adding InitialsAndAge.py from Lectures 2 & 3

---
+++ Classwork/InitialsAndAge.py
@@ -0,0 +1,17 @@
+"""
+Kishan Patel
+CS 100 - Section 019, Fall 2019
+lecture 2 & 3 - September 5th/10th, 2019
+"""
+
+first = input("Please enter your first name: ")
+last = input("Please enter your last name: ")
+age = int(input("Please enter your age: "))
+
+firstInitials = first[0] + first[-1]
+lastInitials = last[0] + last[-1]
+futureAge = first + " " + last + ", in 10 years you will be " + str(age + 10) + " years old."
+
+print("")
+print("Your first and last letters of your full name are:", firstInitials + lastInitials)
+print(futureAge)
