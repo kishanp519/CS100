86febbb277541d0f36e24b105e6f51579264af24
Kishan Patel <kishanp519@gmail.com>
2019-09-17 11:50:21 -0400

    Adding ExamScores.py

---
+++ Classwork/ExamScores.py
@@ -0,0 +1,14 @@
+"""
+Kishan Patel
+CS 100 - Section 019, Fall 2019
+Lecture 5 - September 17th, 2019
+"""
+
+scores = [int(input("Enter your exam grade for Exam #1: ")), int(input("Enter your exam grade for Exam #2: ")),
+          int(input("Enter your exam grade for Exam #3: ")), int(input("Enter your exam grade for Exam #4: ")),
+          int(input("Enter your exam grade for Exam #5: "))]
+
+average = sum(scores) / len(scores)
+
+print("Scores: ", scores)
+print("Average: ", average)
